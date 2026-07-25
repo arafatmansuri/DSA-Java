83c80154aa3fbe3ea47f45dc263b374e4235c04b
arafatmansuri <mansuriarafat302@gmail.com>
2026-07-25 16:20:43 +0530

    Add Inbuilt LinkedList

---
+++ src/LinkedListActions.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class LinkedListActions {
+    LinkedList<Integer> nums = new LinkedList<Integer>();
+
+    public void add(int num){
+        nums.add(num);
+    }
+    public LinkedList<Integer> display(){
+        return nums;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
+import java.util.LinkedList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {5,30,12,10,1,44,11,50,2,3};
-        int[] arr = {3,5,1,4,6,2};
+        LinkedListActions la = new LinkedListActions();
 
-        Sort s = new Sort(numbers);
-
-        s.mergeSort(arr,0,arr.length-1);
-        for (int n : arr){
-            System.out.println(n);
-        }
+        la.add(10);
+        la.add(50);
+        System.out.println(la.display());
     }
 }
